8534aeee0f660e172235ccc22630a5f839896f20
Mr . Taha <bayat.taha2011@gmail.com>
2024-12-17 15:22:04 +0330

    funcs.py

--- funcs.py
+++ funcs.py
@@ -1,14 +1,14 @@
 import aspose.words 
 
-def of() :
+def of() : # Open files function
     g = open("a.txt", "w")
     b = open("b.txt" , "w")
     n = open("n.txt", "w")
     return g, b, n
 
-g, b, n = of()
+g, b, n = of() 
 
-def wif(name, score, g, b, n) :
+def wif(name, score, g, b, n) : # Write in files function
     if score >= 18 :
         g.write(f'{name} : {str(score)}\n')
     elif 14<score<18 :
@@ -17,20 +17,14 @@ def wif(name, score, g, b, n) :
         b.write(f'{name} : {str(score)}\n')
 
 
-def cf(g,n,b) :
+def cf(g,n,b) : # Close files function
     g.close()
     n.close()
     b.close()
 
-def gd(t, g, n, b) :
+def gd(t, g, n, b) : # Get data function
     for i in range(t) :
         ina = input("Enter student name :\n")
         ins = float(input("Enter student score :\n"))
         wif(name = ina, score = ins, g=g , n=n, b=b)
 
-def ttd() :
-    
-    doc = aspose.words.Document(f"n.txt")
-    doc.save("Output.pdf")
-
-ttd()
